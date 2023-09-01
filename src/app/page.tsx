7dec881ddb5f2a9a59b8cd523e74b5d5206e4248
Mauro <mauromaio.dev@gmail.com>
2023-08-31 21:43:45 -0300

    feat: user and repos import

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,14 @@
 import Link from "next/link";
 
-export default function Home() {
+import { Repos } from "components/Repo";
+import { User } from "components/User";
+
+export default async function Home() {
   return (
     <div>
       <h1>Home</h1>
+      <User />
+      <Repos />
       <Link href="/dashboard">Dashboard</Link>
     </div>
   );
