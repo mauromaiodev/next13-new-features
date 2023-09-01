2a8b6074507fb3cde7f72c590014444ce7e1d0a9
Mauro <mauromaio.dev@gmail.com>
2023-08-31 21:44:09 -0300

    feat: repos component with fetch revalidate

---
+++ src/components/Repo.tsx
@@ -0,0 +1,18 @@
+export async function Repos() {
+  const response = await fetch(
+    "https://api.github.com/users/mauromaiodev/repos",
+    {
+      next: {
+        revalidate: 60
+      }
+    }
+  );
+  const repos = await response.json();
+
+  return (
+    <div>
+      <h1>Repos</h1>
+      <pre>{JSON.stringify(repos, null, 2)}</pre>
+    </div>
+  );
+}
