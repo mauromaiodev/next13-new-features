a2ca4f38ee159e5392addac73dac0c8e361c63dc
Mauro <mauromaio.dev@gmail.com>
2023-09-18 18:07:59 -0300

    feat: suspense API testing in app page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Link from "next/link";
+import { Suspense } from "react";
 
 import { Repos } from "components/Repo";
 import { User } from "components/User";
@@ -7,8 +8,12 @@ export default async function Home() {
   return (
     <div>
       <h1>Home</h1>
-      <User />
-      <Repos />
+      <Suspense fallback={<p>Carregando usuário...</p>}>
+        <User />
+      </Suspense>
+      <Suspense fallback={<p>Carregando repositórios...</p>}>
+        <Repos />
+      </Suspense>
       <Link href="/dashboard">Dashboard</Link>
     </div>
   );
